11dd61ca13212ebe4cc041271fcbf1e2b242b5d0
oraschewski <36086882+oraschewski@users.noreply.github.com>
2024-07-04 15:14:43 +0200

    Provide clear example config files.

--- config/config_T2_losar.m
+++ config/config_T2_losar.m
@@ -7,7 +7,7 @@ function cfg = config_T2_losar()
 %
 % Falk Oraschewski, 01.08.2023
 
-%% Mandatory settings
+%% Define survey type
 cfg.typeSurvey      = 'profile';
 
 %% File settings
@@ -22,55 +22,62 @@ cfg.dirGPS      = 'data/raw_data/gps';          % Subpath to GPS data
 cfg.dirSup      = 'data/supplement_data';       % Subpath to any supplementary data
 
 % Filenames
-cfg.fileProc    = 'T2_LOSAR_filtered.mat';      % Processed output data file
-cfg.filePreProc = 'T2_p8_md100.mat';            % Pre-processed data file (after fmcw_range is applied).
-cfg.filePos     = 'T2_pos_smooth.mat';          % Pre-processed positioning data file
-cfg.fileGPS     = 'gpsAll.csv';                 % Input file for GPS data (currently only .csv files are supported with the fields 'filename', 'latitude', 'longitude' and 'elevation').
-cfg.fileDensity = 'densityKCC_LinearlyExtrapolated.csv'; % Density data (currently only .csv files are supported with the fields 'range' and 'density').
+cfg.fileProc    = 'T2_losar.mat';               % File for processed output data
+cfg.filePreProc = 'T2_p8_md100.mat';            % File for pre-processed data (after FMCW signal processing is applied).
+cfg.filePos     = 'T2_pos_smooth.mat';          % File for pre-processed positioning data
+cfg.fileGPS     = 'gpsAll.csv';                 % File for GPS data     (currently only .csv files are supported with the fields 'filename', 'latitude', 'longitude' and 'elevation').
+cfg.fileDensity = 'densityKCC.csv';             % file for density data (currently only .csv files are supported with the fields 'range' and 'density').
 
 % File update settings
-cfg.newPreProc  = false; % Option to update cfg.filePreProc
-cfg.newPos      = false; % Option to update cfg.filePos
-cfg.newProc     = true; % Option to update cfg.fileProc
-cfg.newSlopes   = true; % Option to avoid reprocessing the slope
+cfg.newPreProc  = false;            % Option to update cfg.filePreProc
+cfg.newPos      = false;            % Option to update cfg.filePos
+cfg.newProc     = true;             % Option to update cfg.fileProc
+% LO-SAR specific
+cfg.newSlopes   = false;            % Option to avoid reprocessing the slopes
 
-
-%% Processing settings
-% General settings
-cfg.lowestNoise     = true;  % Option to select burst with lowest noise floor in each trace
-cfg.cropCables      = true;  % Option to correct for cable length
-cfg.correctDensity  = true;  % Option to correct for density 
-cfg.matchShape      = false; % Option to apply a shape matching filter (following Rahman et al., 2014)
-
-% Survey type specific
-cfg.methodSAR       = 'losar';      % Options: 'losar', 'interpolation', 'movmean'
-cfg.lengthSAR       = 5;            % Synthetic aperture length (m)
-cfg.methodPos       = 'smooth';     % Position processing method; Options: 'smooth', 'org'
-cfg.methodSlope     = 'linefit';    % Slope estimation methods; options: 'linefit', 'compute'
-cfg.slopeMax        = 30;           % Slope range boundaries for 'linefit'
-cfg.slopeInterval   = 0.2;          % Slope range interval for 'linefit' 
-cfg.filterSlopes    = true;         % Option to filter slopes using a 2d moving median filter
-cfg.paramSmoothing  = 0.3;          % Smoothing parameter for positioning
-cfg.profileSpacing  = 0.1;          % Equidistant spacing for processed profile 
-cfg.useAntennaPos   = false;        % Estimate antenna positions (currently this has no effect)
-cfg.defineLines     = false;        % Option to process data linewise
-cfg.numLines        = {1};          % Line numbers
-cfg.indLines        = {[1:713]};   % Indices associated with line numbers
-
-%% Survey setup settings
-cfg.maxDepth    = 100;      % MaxDepth for fmcw_range
-cfg.padding     = 8;        % Padding factor for fmcw_range
-cfg.selInd      = 1:713;   % Select subset of input files (Can be used to exclude faulty traces or parts of the data).
-cfg.skipFirst   = 0;        % Skip files at start (performed after selInd)
-cfg.skipLast    = 0;        % Skip files at end (performed after selInd)
-
-% Radar setup
-cfg.cableTx     = 5;    % Cable length for transmitting antenna (m)
-cfg.cableRx     = 5;    % Cable length for receiving antenna (m)
-cfg.distAntenna = 2.7;  % Antenna seperation (m)
+%% General processing settings
+% Processing options
+cfg.lowestNoise     = true;         % Option to select burst with lowest noise floor in each trace
+cfg.cropCables      = true;         % Option to correct for cable length
+cfg.correctDensity  = true;         % Option to correct for firn density 
+cfg.matchShape      = false;        % Option to apply a shape matching filter (following Rahman et al., 2014)
 
 % Position settings
-cfg.useGPS = true;      % Use GPS data
-cfg.typeGPS = 'csv';    % GPS input type, options: 'csv'
-cfg.surveyEPSG = 2056;  % EPSG code for Cartesian coordinate system for the project
+cfg.useGPS          = true;         % Load GPS data associated with .dat-files
+cfg.typeGPS         = 'csv';        % GPS input type, options: 'csv'
+cfg.surveyEPSG      = 2056;         % EPSG code for Cartesian coordinate system for the project
+
+% FMCW signal processing settings
+cfg.maxDepth        = 100;          % MaxDepth for fmcw_range [m]
+cfg.padding         = 8;            % Padding factor for fmcw_range
+
+% File selection settings
+cfg.selInd          = 1:713;        % Select subset of input files (Can be used to exclude faulty traces or parts of the data).
+cfg.skipFirst       = 0;            % Skip files at start (performed after selInd)
+cfg.skipLast        = 0;            % Skip files at end (performed after selInd)
+
+% Radar setup
+cfg.cableTx         = 5;            % Cable length for transmitting antenna [m]
+cfg.cableRx         = 5;            % Cable length for receiving antenna [m]
+cfg.distAntenna     = 2.7;          % Antenna seperation [m]
+
+%% Survey type specific settings
+% Processing options
+cfg.methodProc      = 'losar';      % Options: 'losar', 'interpolation', 'movmean'
+cfg.methodPos       = 'smooth';     % Position processing method; Options: 'smooth', 'org'
+cfg.methodSlope     = 'linefit';    % Slope estimation methods; options: 'linefit', 'compute'
+cfg.filterSlopes    = true;         % Option to filter slopes using a 2d moving median filter
+
+% Processing settings
+cfg.lengthSAR       = 5;            % Synthetic aperture length [m]
+cfg.profileSpacing  = 0.1;          % Equidistant spacing for processed profile [m]
+cfg.posSmoothing    = 0.3;          % Smoothing parameter for profile positioning
+cfg.slopeMax        = 30;           % Slope range boundaries for 'linefit' [degree]
+cfg.slopeInterval   = 0.2;          % Slope range interval for 'linefit' [degree]
+cfg.slopeFilterWin  = [40 10];      % Window for slope filtering [bins].
+
+% Linewise processing
+% cfg.defineLines     = false;        % Option to process data linewise
+% cfg.numLines        = {1};          % Line numbers
+% cfg.indLines        = {[1:713]};   % Indices associated with line numbers
 end
--- config/config_T2_raw.m
+++ config/config_T2_raw.m
@@ -7,7 +7,7 @@ function cfg = config_T2_raw()
 %
 % Falk Oraschewski, 01.08.2023
 
-%% Mandatory settings
+%% Define survey type
 cfg.typeSurvey      = 'profile';
 
 %% File settings
@@ -22,53 +22,55 @@ cfg.dirGPS      = 'data/raw_data/gps';          % Subpath to GPS data
 cfg.dirSup      = 'data/supplement_data';       % Subpath to any supplementary data
 
 % Filenames
-cfg.fileProc    = 'T2_raw.mat';                 % Processed output data file
-cfg.filePreProc = 'T2_p8_md100.mat';            % Pre-processed data file (after fmcw_range is applied).
-cfg.filePos     = 'T2_pos_smooth.mat';          % Pre-processed positioning data file
-cfg.fileGPS     = 'gpsAll.csv';                 % Input file for GPS data (currently only .csv files are supported with the fields 'filename', 'latitude', 'longitude' and 'elevation').
-cfg.fileDensity = 'densityKCC_LinearlyExtrapolated.csv'; % Density data (currently only .csv files are supported with the fields 'range' and 'density').
+cfg.fileProc    = 'T2_interp.mat';              % File for processed output data
+cfg.filePreProc = 'T2_p8_md100.mat';            % File for pre-processed data (after FMCW signal processing is applied).
+cfg.filePos     = 'T2_pos_smooth.mat';          % File for pre-processed positioning data
+cfg.fileGPS     = 'gpsAll.csv';                 % File for GPS data     (currently only .csv files are supported with the fields 'filename', 'latitude', 'longitude' and 'elevation').
+cfg.fileDensity = 'densityKCC.csv';             % file for density data (currently only .csv files are supported with the fields 'range' and 'density').
 
 % File update settings
-cfg.newPreProc  = false; % Option to update cfg.filePreProc
-cfg.newPos      = false; % Option to update cfg.filePos
-cfg.newProc     = true; % Option to update cfg.fileProc
-cfg.newSlopes   = false; % Option to avoid reprocessing the slope
+cfg.newPreProc  = false;            % Option to update cfg.filePreProc
+cfg.newPos      = false;            % Option to update cfg.filePos
+cfg.newProc     = true;             % Option to update cfg.fileProc
 
-
-%% Processing settings
-% General settings
-cfg.lowestNoise     = true;  % Option to select burst with lowest noise floor in each trace
-cfg.cropCables      = true;  % Option to correct for cable length
-cfg.correctDensity  = true;  % Option to correct for density 
-cfg.matchShape      = false; % Option to apply a shape matching filter (following Rahman et al., 2014)
-
-% Survey type specific
-cfg.methodSAR       = 'interpolation';      % Options: 'losar', 'interpolation', 'movmean'
-cfg.lengthSAR       = 5;            % Synthetic aperture length (m)
-cfg.methodPos       = 'smooth';     % Position processing method; Options: 'smooth', 'org'
-cfg.methodSlope     = 'linefit';    % Slope estimation methods; options: 'linefit', 'compute' 
-cfg.filterSlopes    = true;         % Option to filter slopes using a 2d moving median filter
-cfg.paramSmoothing  = 0.3;          % Smoothing parameter for positioning
-cfg.profileSpacing  = 0.1;          % Equidistant spacing for processed profile 
-cfg.useAntennaPos   = false;        % Estimate antenna positions (currently this has no effect)
-cfg.defineLines     = false;        % Option to process data linewise
-cfg.numLines        = {1};          % Line numbers
-cfg.indLines        = {[1:713]};   % Indices associated with line numbers
-
-%% Survey setup settings
-cfg.maxDepth    = 100;      % MaxDepth for fmcw_range
-cfg.padding     = 8;        % Padding factor for fmcw_range
-cfg.selInd      = 1:713;   % Select subset of input files (Can be used to exclude faulty traces or parts of the data).
-cfg.skipFirst   = 0;        % Skip files at start (performed after selInd)
-cfg.skipLast    = 0;        % Skip files at end (performed after selInd)
-
-% Radar setup
-cfg.cableTx     = 5;    % Cable length for transmitting antenna (m)
-cfg.cableRx     = 5;    % Cable length for receiving antenna (m)
-cfg.distAntenna = 2.7;  % Antenna seperation (m)
+%% General processing settings
+% Processing options
+cfg.lowestNoise     = true;         % Option to select burst with lowest noise floor in each trace
+cfg.cropCables      = true;         % Option to correct for cable length
+cfg.correctDensity  = true;         % Option to correct for firn density 
+cfg.matchShape      = false;        % Option to apply a shape matching filter (following Rahman et al., 2014)
 
 % Position settings
-cfg.useGPS = true;      % Use GPS data
-cfg.typeGPS = 'csv';    % GPS input type, options: 'csv'
-cfg.surveyEPSG = 2056;  % EPSG code for Cartesian coordinate system for the project
+cfg.useGPS          = true;         % Load GPS data associated with .dat-files
+cfg.typeGPS         = 'csv';        % GPS input type, options: 'csv'
+cfg.surveyEPSG      = 2056;         % EPSG code for Cartesian coordinate system for the project
+
+% FMCW signal processing settings
+cfg.maxDepth        = 100;          % MaxDepth for fmcw_range [m]
+cfg.padding         = 8;            % Padding factor for fmcw_range
+
+% File selection settings
+cfg.selInd          = 1:713;        % Select subset of input files (Can be used to exclude faulty traces or parts of the data).
+cfg.skipFirst       = 0;            % Skip files at start (performed after selInd)
+cfg.skipLast        = 0;            % Skip files at end (performed after selInd)
+
+% Radar setup
+cfg.cableTx         = 5;            % Cable length for transmitting antenna [m]
+cfg.cableRx         = 5;            % Cable length for receiving antenna [m]
+cfg.distAntenna     = 2.7;          % Antenna seperation [m]
+
+%% Survey type specific settings
+% Processing options
+cfg.methodProc      = 'interp';     % Options: 'losar', 'interpolation', 'movmean'
+cfg.methodPos       = 'smooth';     % Position processing method; Options: 'smooth', 'org'
+
+% Processing settings
+cfg.lengthSAR       = 5;            % Synthetic aperture length [m]
+cfg.profileSpacing  = 0.1;          % Equidistant spacing for processed profile [m]
+cfg.posSmoothing    = 0.3;          % Smoothing parameter for profile positioning
+
+% Linewise processing
+% cfg.defineLines     = false;        % Option to process data linewise
+% cfg.numLines        = {1};          % Line numbers
+% cfg.indLines        = {[1:713]};   % Indices associated with line numbers
 end
